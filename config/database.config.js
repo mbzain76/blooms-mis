29899151784a1034b958f2aea427477a5a402440
Brian Gacheru <briangach98@gmail.com>
2019-03-24 22:47:42 -0700

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-    url: 'mongodb://gacheru:fallingskies123@ds211694.mlab.com:11694/school_system'
-}
+
+
+// config/index.js
+const dbuser = 'gacheru';
+const dbpassword = 'fallingskies123';
+
+const MONGODB_URI = `mongodb://${dbuser}:${dbpassword}@ds211694.mlab.com:11694/school_system`;
+
+module.exports = MONGODB_URI;
