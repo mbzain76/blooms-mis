b61e22197ce4beac5e503dc92d0e692323fb31ea
Brian Gacheru <briangach98@gmail.com>
2019-03-25 02:12:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,21 +11,6 @@ const path = require("path")
 app.use(express.static(path.join(__dirname, "client", "build")))
 
 
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');    
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
-
 // Configuring the database
 const MONGODB_URI = require('./config/database.config.js');
 const mongoose = require('mongoose');
@@ -43,20 +28,10 @@ mongoose.connect(MONGODB_URI, {
 });
 
 
-
-
-//Require the students routes
-require('./routes/student.routes.js')(app);
-
+// ...
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-
-
-// listen for requests
-app.listen(port, () => {
-    console.log("Server is listening on port " + port);
-});
-
+app.listen(...)
