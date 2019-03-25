634c45e8cd66711080462bd2441e19a44cac1b68
Brian Gacheru <briangach98@gmail.com>
2019-03-24 23:25:16 -0700

    Update AboutGaurdian.js

--- client/src/about-pages/AboutGaurdian.js
+++ client/src/about-pages/AboutGaurdian.js
@@ -58,7 +58,7 @@ class AboutGaurdian extends Component{
 
    componentDidMount(){
        
-        axios.get(`http://localhost:5000/students/${id}`).then(res =>{
+        axios.get(`/students/${id}`).then(res =>{
             console.log(res)
             this.setState({
              studentDet: res.data
