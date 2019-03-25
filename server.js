48ddc2a3048f97d916b0638ed7d19cb10eb3f59c
Brian Gacheru <briangach98@gmail.com>
2019-03-25 01:05:48 -0700

    axios url

--- server.js
+++ server.js
@@ -48,6 +48,16 @@ mongoose.connect(MONGODB_URI, {
 //Require the students routes
 require('./routes/student.routes.js')(app);
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build')); // serve the static react app
+  app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  });
+  console.log('Serving React App...');
+};
+
+
 //add this:
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
